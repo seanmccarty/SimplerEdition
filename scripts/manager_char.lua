e8a55f9fce76e0dfbf26ecc4beee8018ac5537b0
Sean McCarty <28798809+seanmccarty@users.noreply.github.com>
2022-06-15 00:53:19 +0900

    Update charManager for JulyBeta

--- scripts/manager_char.lua
+++ scripts/manager_char.lua
@@ -1,289 +1,10 @@
 function onInit()
-	CharManager.addClassRef = addClassRef;
-	CharManager.addSkillRef = addSkillRef;
-	CharManager.getClassHDUsage = getClassHDUsage;
+	CharManager.addSkill = addSkill;
 	CharManager.resetHealth = resetHealth;
-	CharManager.addClassProficiencyDB = addClassProficiencyDB;
-	CharManager.onClassSkillSelect = onClassSkillSelect;
+	CharManager.onSkillSelect = onSkillSelect;
 end
 
-function addClassRef(nodeChar, sClass, sRecord, bWizard)
-	local nodeSource = CharManager.resolveRefNode(sRecord)
-	if not nodeSource then
-		return;
-	end
-
-	-- Get the list we are going to add to
-	local nodeList = nodeChar.createChild("classes");
-	if not nodeList then
-		return;
-	end
-
-	
-	-- Notify
-	CharManager.outputUserMessage("char_abilities_message_classadd", DB.getValue(nodeSource, "name", ""), DB.getValue(nodeChar, "name", ""));
-
-	-- Keep some data handy for comparisons
-	local sClassName = DB.getValue(nodeSource, "name", "");
-	local sClassNameLower = StringManager.trim(sClassName):lower();
-
-	-- Check to see if the character already has this class; or create a new class entry
-	local nodeClass = nil;
-	for _,v in pairs(nodeList.getChildren()) do
-		local sExistingClassName = StringManager.trim(DB.getValue(v, "name", "")):lower();
-		if (sExistingClassName == sClassNameLower) and (sExistingClassName ~= "") then
-			nodeClass = v;
-			break;
-		end
-	end
-	local bExistingClass = false;
-	if nodeClass then
-		bExistingClass = true;
-	else
-		if nodeList.getChildCount() == 0 then
-			nodeClass = nodeList.createChild();
-		else
-			function SystemMessage(sText)
-				local msg = {font = "systemfont"};
-				msg.text = sText;
-				Comm.addChatMessage(msg);
-			end
-			CharManager.outputUserMessage("SE_char_abilities_message_classadd_abort",DB.getValue(nodeChar, "name", ""));
-			return;
-		end
-		
-	end
-	
-	-- Calculate current spell slots before levelling up
-	local nCasterLevel = CharManager.calcSpellcastingLevel(nodeChar);
-	local nPactMagicLevel = CharManager.calcPactMagicLevel(nodeChar);
-	
-	-- Any way you get here, overwrite or set the class reference link with the most current
-	DB.setValue(nodeClass, "shortcut", "windowreference", sClass, sRecord);
-	
-	-- Add basic class information
-	local nLevel = 1;
-	if bExistingClass then
-		nLevel = DB.getValue(nodeClass, "level", 1) + 1;
-	else
-		DB.setValue(nodeClass, "name", "string", sClassName);
-		local aDice = {};
-		-- for i = 1, nHDMult do
-		 	table.insert(aDice, "d6");
-		-- end
-		DB.setValue(nodeClass, "hddie", "dice", aDice);
-		
-		--load stat values if new class
-		DB.setValue(nodeChar,"abilities.strength.score","number",tonumber(DB.getValue(nodeSource, "stat.str", "0")))
-		DB.setValue(nodeChar,"abilities.dexterity.score","number",tonumber(DB.getValue(nodeSource, "stat.dex", "0")))
-		DB.setValue(nodeChar,"abilities.constitution.score","number",tonumber(DB.getValue(nodeSource, "stat.con", "0")))
-		DB.setValue(nodeChar,"abilities.intelligence.score","number",tonumber(DB.getValue(nodeSource, "stat.int", "0")))
-		DB.setValue(nodeChar,"abilities.wisdom.score","number",tonumber(DB.getValue(nodeSource, "stat.wis", "0")))
-		DB.setValue(nodeChar,"abilities.charisma.score","number",tonumber(DB.getValue(nodeSource, "stat.cha", "0")))
-	end
-	DB.setValue(nodeClass, "level", "number", nLevel);
-	
-
-	-- Calculate total level
-	local nTotalLevel = 0;
-	for _,vClass in pairs(nodeList.getChildren()) do
-		nTotalLevel = nTotalLevel + DB.getValue(vClass, "level", 0);
-	end
-	
-	-- Add hit points based on level added
-	--SE
-	local nPerLevelHP = tonumber(DB.getValue(nodeSource, "hp.hitperlevel", "0")) or 0;
-	local nHP = nTotalLevel * nPerLevelHP;
-
-	CharManager.outputUserMessage("char_abilities_message_hpaddmax", DB.getValue(nodeSource, "name", ""), DB.getValue(nodeChar, "name", ""), nPerLevelHP);
-	DB.setValue(nodeChar, "hp.total", "number", nHP);
-
-	--SE Armor Class (transform from total AC to modifier to base of 10)
-	local nACmod = tonumber(DB.getValue(nodeSource, "AC", "0"));
-	nACmod = nACmod - 10;
-	DB.setValue(nodeChar, "defenses.ac.armor", "number", nACmod);
-	DB.setValue(nodeChar, "defenses.ac.dexbonus", "string", "no");
-
-	--SE profIncrease
-	local nProfMod = tonumber(DB.getValue(nodeSource, "profIncrease", "0"));
-	DB.setValue(nodeChar, "profIncrease", "number", nProfMod);
-
-	-- Add proficiencies
-	if not bExistingClass and not bWizard then
-		if nTotalLevel == 1 then
-			for _,v in pairs(DB.getChildren(nodeSource, "proficiencies")) do
-				CharManager.addClassProficiencyDB(nodeChar, "reference_classproficiency", v.getPath());
-			end
-		else
-			for _,v in pairs(DB.getChildren(nodeSource, "multiclassproficiencies")) do
-				CharManager.addClassProficiencyDB(nodeChar, "reference_classproficiency", v.getPath());
-			end
-		end
-	end
-	
-	-- Determine whether a specialization is added this level
-	if not bWizard then
-		local nodeSpecializationFeature = nil;
-		local tClassSpecOptions = {};
-		for _,v in pairs(DB.getChildren(nodeSource, "features")) do
-			if (DB.getValue(v, "level", 0) == nLevel) and (DB.getValue(v, "specializationchoice", 0) == 1) then
-				nodeSpecializationFeature = v;
-				tClassSpecOptions = CharManager.getClassSpecializationOptions(nodeSource);
-				break;
-			end
-		end
-		
-		-- Add features, with customization based on whether specialization is added this level
-		local rClassAdd = { nodeChar = nodeChar, nodeSource = nodeSource, nLevel = nLevel, nodeClass = nodeClass, nCasterLevel = nCasterLevel, nPactMagicLevel = nPactMagicLevel };
-		if #tClassSpecOptions == 0 then
-			CharManager.addClassFeatureHelper(nil, rClassAdd);
-		elseif #tClassSpecOptions == 1 then
-			CharManager.addClassFeatureHelper( { tClassSpecOptions[1].text }, rClassAdd);
-		else
-			-- Display dialog to choose specialization
-			local wSelect = Interface.openWindow("select_dialog", "");
-			local sTitle = Interface.getString("char_build_title_selectspecialization");
-			local sMessage = string.format(Interface.getString("char_build_message_selectspecialization"), DB.getValue(nodeSpecializationFeature, "name", ""), 1);
-			wSelect.requestSelection (sTitle, sMessage, tClassSpecOptions, CharManager.addClassFeatureHelper, rClassAdd);
-		end
-	else
-		return nodeClass;
-	end
-	--SE set spell slots
-	local newSpellSlot = CharManager.calcSpellcastingLevel(nodeChar);
-	local newPactSlot = CharManager.calcPactMagicLevel(nodeChar);
-	local totalSlots = newSpellSlot + newPactSlot + DB.getValue(nodeSource, "initialSpellSlots", 1) - 1;
-	if newPactSlot>0 then
-		DB.setValue(nodeChar, "powermeta.pactmagicslots1.max", "number", totalSlots);
-		--if we have slots, set the spell slots for 2-5 to have a value so the power groups are visible in combat mode
-		if totalSlots > 0 then
-			DB.setValue(nodeChar, "powermeta.pactmagicslots2.max", "number", 1);
-			DB.setValue(nodeChar, "powermeta.pactmagicslots3.max", "number", 1);
-			DB.setValue(nodeChar, "powermeta.pactmagicslots4.max", "number", 1);
-			DB.setValue(nodeChar, "powermeta.pactmagicslots5.max", "number", 1);
-		end
-	else
-		DB.setValue(nodeChar, "powermeta.spellslots1.max", "number", totalSlots);
-		--if we have slots, set the spell slots for 2-5 to have a value so the power groups are visible in combat mode
-		if totalSlots > 0 then
-			DB.setValue(nodeChar, "powermeta.spellslots2.max", "number", 1);
-			DB.setValue(nodeChar, "powermeta.spellslots3.max", "number", 1);
-			DB.setValue(nodeChar, "powermeta.spellslots4.max", "number", 1);
-			DB.setValue(nodeChar, "powermeta.spellslots5.max", "number", 1);
-		end
-	end
-	
-	
-
-
-	--Add the new spells for the class
-	local nLevel = nTotalLevel;
-	local sClass = sClassName;
-	local aMappings = LibraryData.getMappings("spell");
-	for _,vMapping in ipairs(aMappings) do
-		for _,vSpell in pairs(DB.getChildrenGlobal(vMapping)) do
-			local sSpell = StringManager.trim(DB.getValue(vSpell, "name", "")):lower();
-			sSpell = StringManager.trim(sSpell);
-
-			local nSpellLevel = DB.getValue(vSpell, "level", 0);
-			local aSpellSource = StringManager.split(DB.getValue(vSpell, "source", ""), ",");
-
-			for _,vSource in pairs(aSpellSource) do
-				vSource = vSource:lower():gsub("%(optional%)", "")
-				vSource = vSource:lower():gsub("%(new%)", "")
-				vSource = StringManager.trim(vSource);
-				if nSpellLevel == nLevel or (nLevel == 1 and nSpellLevel == 0) then
-					if StringManager.trim(vSource:lower()) == sClass:lower() then
-						PowerManager.addPower("reference_spell", vSpell.getPath(), nodeChar, Interface.getString("power_label_groupspells"));
-					end
-				end
-			end
-
-		end
-	end	
-end
-
-function addClassProficiencyDB(nodeChar, sClass, sRecord)
-	local nodeSource = CharManager.resolveRefNode(sRecord);
-	if not nodeSource then
-		return;
-	end
-	
-	local sText = DB.getValue(nodeSource, "text", "");
-	if sText == "" then
-		return;
-	end
-	
-	local sType = nodeSource.getName();
-	
-	-- Armor, Weapon or Tool Proficiencies
-	if StringManager.contains({"armor", "weapons", "tools"}, sType) then
-		local sText = DB.getText(nodeSource, "text","");
-		CharManager.addProficiencyDB(nodeChar, sType, sText);
-		
-	-- Saving Throw Proficiencies
-	elseif sType == "savingthrows" then
-		local sText = DB.getText(nodeSource, "text", "");
-		for sProf in string.gmatch(sText, "(%a[%a%s]+)%,?") do
-			local sProfLower = StringManager.trim(sProf:lower());
-			if StringManager.contains(DataCommon.abilities, sProfLower) then
-				DB.setValue(nodeChar, "abilities." .. sProfLower .. ".saveprof", "number", 1);
-				CharManager.outputUserMessage("char_abilities_message_saveadd", sProf, DB.getValue(nodeChar, "name", ""));
-			end
-		end
-
-	-- Skill Proficiencies
-	elseif sType == "skills" then
-		-- Parse the skill choice text
-		local sText = DB.getText(nodeSource, "text","");
-		
-		local aSkills = {};
-		local sPicks;
-		--
-		
-
-		--
-
-		if sText:match("Choose any ") then
-			sPicks = sText:match("Choose any (%w+)");
-			
-		elseif sText:match("Choose ") then
-			sPicks = sText:match("Choose (%w+) ");
-			
-			sText = sText:gsub("Choose (%w+) from ", "");
-			sText = sText:gsub("Choose (%w+) skills? from ", "");
-			sText = sText:gsub("and ", "");
-			sText = sText:gsub("or ", "");
-			
-			for sSkill in sText:gmatch("(%a[%a%s]+)%,?") do
-				local sTrim = StringManager.trim(sSkill);
-				table.insert(aSkills, sTrim);
-			end
-		end
-		
-		local nPicks = CharManager.convertSingleNumberTextToNumber(sPicks);
-		
-		if nPicks == 0 then
-			sText = sText:gsub("and ", "");
-			sText = sText:gsub("or ", "");
-			for sSkill in sText:gmatch("(%a[%a%s]+)%,?") do
-				local sTrim = StringManager.trim(sSkill);
-				table.insert(aSkills, sTrim);
-			end
-			if aSkills then 
-				addSkills(nodeChar, aSkills);
-				return;
-			else
-				CharManager.outputUserMessage("char_error_addskill");
-				return nil;
-			end
-		end
-
-		CharManager.pickSkills(nodeChar, aSkills, nPicks);
-	end
-end
-
-function onClassSkillSelect(aSelection, rSkillAdd)
+function onSkillSelect(aSelection, rSkillAdd)
 	-- For each selected skill, add it to the character
 	for _,sSkill in ipairs(aSelection) do
 		addSimpleSkillDB(rSkillAdd.nodeChar, sSkill, rSkillAdd.nProf or 1);
@@ -306,7 +27,13 @@ function addSkills(nodeChar, aSkills)
 	for k,v in ipairs(aSkills) do
 		local rSkillData = DataCommon.skilldata[v];
 		if rSkillData then
-			aSkills[k] = { text = v, linkclass = "reference_skill", linkrecord = "reference.skilldata." .. rSkillData.lookup .. "@*" };
+			local rSkill = { text = v, linkclass = "", linkrecord = "" };
+			local nodeSkill = RecordManager.findRecordByStringI("skill", "name", v);
+			if nodeSkill then
+				rSkill.linkclass = "reference_skill";
+				rSkill.linkrecord = DB.getPath(nodeSkill);
+			end
+			aSkills[k] = rSkill;
 		end
 	end
 	
@@ -352,8 +79,8 @@ function addSimpleSkillDB(nodeChar, sSkill, linkclass, linkrecord)
 	return nodeSkill;
 end
 
-function addSkillRef(nodeChar, sClass, sRecord)
-	local nodeSource = CharManager.resolveRefNode(sRecord);
+function addSkill(nodeChar, sClass, sRecord)
+	local nodeSource = DB.findNode(sRecord);
 	if not nodeSource then
 		return;
 	end
@@ -364,17 +91,6 @@ function addSkillRef(nodeChar, sClass, sRecord)
 		local nodeSkill = SECharManager.addSimpleSkillDB(nodeChar, DB.getValue(nodeSource, "name", ""));
 		DB.setValue(nodeSkill, "text", "formattedtext", DB.getValue(nodeSource, "text", ""));
 	end
-end
-
-function getClassHDUsage(nodeChar)
-	local nHD = math.max(1,DB.getValue(nodeChar,"abilities.constitution.bonus",0));
-	local nHDUsed = 0;
-	
-	for _,nodeChild in pairs(DB.getChildren(nodeChar, "classes")) do
-		nHDUsed = nHDUsed + DB.getValue(nodeChild, "hdused", 0);
-	end
-	
-	return nHDUsed, nHD;
 end
 
 function resetHealth(nodeChar, bLong)
@@ -429,7 +145,7 @@ function resetHealth(nodeChar, bLong)
 
 	-- Reset half or quarter of hit dice (assume biggest hit dice selected first)
 	if bResetHalfHitDice or bResetQuarterHitDice then
-		local nHDUsed, nHDTotal = getClassHDUsage(nodeChar);
+		local nHDUsed, nHDTotal = CharClassManager.getCharClassHDUsage(nodeChar);
 		if nHDUsed > 0 then
 			local nHDRecovery;
 			if bResetQuarterHitDice then
---
+++ scripts/manager_char_class.lua
@@ -0,0 +1,186 @@
+local oAddClass;
+
+function onInit()
+	CharClassManager.getCharClassHDUsage = getCharClassHDUsage;
+	CharClassManager.helperAddClassHP = helperAddClassHP;
+	CharClassManager.helperAddClassUpdateSpellSlots = helperAddClassUpdateSpellSlots;
+	CharClassManager.addClassProficiency = addClassProficiency; 
+	oAddClass = CharClassManager.addClass;
+	CharClassManager.addClass = addClass;
+end
+
+---Overrides the the calculation for how many hit dice a character will have to be the CON bonus (minimum 1)
+---@param nodeChar any the character node of interest
+---@return integer nHDUsed number of hit dice used
+---@return number nHD max number of hit dice for the character
+function getCharClassHDUsage(nodeChar)
+	local nHD = math.max(1,DB.getValue(nodeChar,"abilities.constitution.bonus",0));
+	local nHDUsed = 0;
+	
+	for _,nodeChild in pairs(DB.getChildren(nodeChar, "classes")) do
+		nHDUsed = nHDUsed + DB.getValue(nodeChild, "hdused", 0);
+	end
+	
+	return nHDUsed, nHD;
+end
+
+---Hit points added per level for a given class are pre-determined and all hit dice are d6
+---@param rAdd any the rAdd structure for the character
+function helperAddClassHP(rAdd)
+	if rAdd.bNewCharClass then
+		DB.setValue(rAdd.nodeCharClass, "hddie", "dice", "1d6");
+	end
+
+	-- Add hit points based on class leveled
+	local nHP = DB.getValue(rAdd.nodeChar, "hp.total", 0);
+	local nPerLevelHP = tonumber(DB.getValue(rAdd.nodeSource, "hp.hitperlevel", "0")) or 0;
+	nHP = nHP + nPerLevelHP;
+
+	CharManager.outputUserMessage("char_abilities_message_hpaddmax", rAdd.sSourceName, rAdd.sCharName, nPerLevelHP);
+	DB.setValue(rAdd.nodeChar, "hp.total", "number", nHP);
+end
+
+---Available spells slots are calculated differently for SE and are all of one type
+---@param rAdd any the rAdd structure for the character
+function helperAddClassUpdateSpellSlots(rAdd)
+	--SE set spell slots
+	--Get spells slots for the new level (rAdd has the count before the upgrade happens)
+	local tCharClassMagicData = CharClassManager.getCharClassMagicData(rAdd.nodeChar);
+
+	--if nSpellClasses > 0 it means the ability to cast spells is present
+	if tCharClassMagicData.nSpellClasses > 0 then
+		--if there is spellcasting, it is the number of initial spell slots + the number of slots above first level
+		local nTotalSlots = rAdd.nCharClassLevel + DB.getValue(rAdd.nodeSource, "initialSpellSlots", 1) - 1;
+		local bPactmagic = tCharClassMagicData[1].bPactmagic;
+		if bPactmagic then
+			DB.setValue(rAdd.nodeChar, "powermeta.pactmagicslots1.max", "number", nTotalSlots);
+			--if we have slots, set the spell slots for 2-5 to have a value so the power groups are visible in combat mode. The xml for these is invisible though so you don't see the indicators.
+			DB.setValue(rAdd.nodeChar, "powermeta.pactmagicslots2.max", "number", 1);
+			DB.setValue(rAdd.nodeChar, "powermeta.pactmagicslots3.max", "number", 1);
+			DB.setValue(rAdd.nodeChar, "powermeta.pactmagicslots4.max", "number", 1);
+			DB.setValue(rAdd.nodeChar, "powermeta.pactmagicslots5.max", "number", 1);
+		else
+			DB.setValue(rAdd.nodeChar, "powermeta.spellslots1.max", "number", nTotalSlots);
+			--if we have slots, set the spell slots for 2-5 to have a value so the power groups are visible in combat mode. The xml for these is invisible though so you don't see the indicators.
+			DB.setValue(rAdd.nodeChar, "powermeta.spellslots2.max", "number", 1);
+			DB.setValue(rAdd.nodeChar, "powermeta.spellslots3.max", "number", 1);
+			DB.setValue(rAdd.nodeChar, "powermeta.spellslots4.max", "number", 1);
+			DB.setValue(rAdd.nodeChar, "powermeta.spellslots5.max", "number", 1);
+		end
+	end	
+
+	--Add the new spells for the class
+	local aMappings = LibraryData.getMappings("spell");
+	for _,vMapping in ipairs(aMappings) do
+		for _,vSpell in pairs(DB.getChildrenGlobal(vMapping)) do
+			local sSpell = StringManager.trim(DB.getValue(vSpell, "name", "")):lower();
+			sSpell = StringManager.trim(sSpell);
+
+			local nSpellLevel = DB.getValue(vSpell, "level", 0);
+			local aSpellSource = StringManager.split(DB.getValue(vSpell, "source", ""), ",");
+
+			for _,vSource in pairs(aSpellSource) do
+				vSource = vSource:lower():gsub("%(optional%)", "")
+				vSource = vSource:lower():gsub("%(new%)", "")
+				vSource = StringManager.trim(vSource);
+				if nSpellLevel == rAdd.nCharClassLevel or (rAdd.nCharClassLevel == 1 and nSpellLevel == 0) then
+					if StringManager.trim(vSource:lower()) ==  rAdd.sSourceName:lower() then
+						PowerManager.addPower("reference_spell", vSpell.getPath(), rAdd.nodeChar, Interface.getString("power_label_groupspells"));
+					end
+				end
+			end
+		end
+	end
+end
+
+---The only changed portion is the skill as indicated by the comment
+---@param nodeChar any the character that the class is being added to
+---@param sClass string the type of the record being added
+---@param sRecord string the record being added
+---@param bWizard boolean if this is added by char wizard
+---@return nil
+function addClassProficiency(nodeChar, sClass, sRecord, bWizard)
+	local rAdd = CharManager.helperBuildAddStructure(nodeChar, sClass, sRecord, bWizard);
+	if not rAdd then
+		return;
+	end
+
+	-- Saving Throw Proficiencies
+	if rAdd.sSourceType == "savingthrows" then
+		local sText = StringManager.trim(DB.getText(rAdd.nodeSource, "text", ""));
+		if sText == "" then
+			return;
+		end
+
+		for sProf in sText:gmatch("(%a[%a%s]+)%,?") do
+			local sProfLower = StringManager.trim(sProf):lower();
+			if StringManager.contains(DataCommon.abilities, sProfLower) then
+				DB.setValue(rAdd.nodeChar, "abilities." .. sProfLower .. ".saveprof", "number", 1);
+				CharManager.outputUserMessage("char_abilities_message_saveadd", sProf, rAdd.sCharName);
+			end
+		end
+	end
+
+	if not rAdd.bWizard then
+		-- Armor, Weapon or Tool Proficiencies
+		if StringManager.contains({"armor", "weapons", "tools"}, rAdd.sSourceType) then
+			local sText = StringManager.trim(DB.getText(rAdd.nodeSource, "text", ""));
+			if sText == "" then
+				return;
+			end
+
+			CharManager.addProficiency(rAdd.nodeChar, rAdd.sSourceType, DB.getText(rAdd.nodeSource, "text", ""));
+			
+
+		-- Skill Proficiencies
+		elseif rAdd.sSourceType == "skills" then
+			local nPicks, tSkills = CharManager.parseSkillProficiencyText(rAdd.nodeSource);
+			if nPicks == 0 then
+				--SE uses non-standard skill text and there are no picks.
+				local sText = DB.getText(rAdd.nodeSource, "text","");
+				local aSkills = CharManager.parseSkillsFromString(sText);
+				if aSkills then 
+					SECharManager.addSkills(rAdd.nodeChar, aSkills);
+					return;
+				else
+					CharManager.outputUserMessage("char_error_addskill");
+					return nil;
+				end
+			end
+			CharManager.pickSkills(rAdd.nodeChar, tSkills, nPicks);
+		end
+	end
+end
+
+---Overrides the addClass so we can intercept multi-classing. If it is not multi-classing, add the class then add the extra preset data that SE has
+---@param nodeChar any the character that the class is being added to
+---@param sClass string the type of the record being added
+---@param sRecord string the record being added
+---@param bWizard boolean if this is added by char wizard
+function addClass(nodeChar, sClass, sRecord, bWizard)
+	local nodeSource = DB.findNode(sRecord);
+	local sClassName = StringManager.trim(DB.getValue(nodeSource, "name", ""));
+	if CharClassManager.getCharLevel(nodeChar) == CharClassManager.getCharClassLevel(nodeChar, sClassName) then
+		oAddClass(nodeChar, sClass, sRecord, bWizard);
+
+		DB.setValue(nodeChar,"abilities.strength.score","number",tonumber(DB.getValue(nodeSource, "stat.str", "0")))
+		DB.setValue(nodeChar,"abilities.dexterity.score","number",tonumber(DB.getValue(nodeSource, "stat.dex", "0")))
+		DB.setValue(nodeChar,"abilities.constitution.score","number",tonumber(DB.getValue(nodeSource, "stat.con", "0")))
+		DB.setValue(nodeChar,"abilities.intelligence.score","number",tonumber(DB.getValue(nodeSource, "stat.int", "0")))
+		DB.setValue(nodeChar,"abilities.wisdom.score","number",tonumber(DB.getValue(nodeSource, "stat.wis", "0")))
+		DB.setValue(nodeChar,"abilities.charisma.score","number",tonumber(DB.getValue(nodeSource, "stat.cha", "0")))
+
+		--SE Armor Class (transform from total AC to modifier to base of 10)
+		local nACmod = tonumber(DB.getValue(nodeSource, "AC", "0"));
+		nACmod = nACmod - 10;
+		DB.setValue(nodeChar, "defenses.ac.armor", "number", nACmod);
+		DB.setValue(nodeChar, "defenses.ac.dexbonus", "string", "no");
+
+		--SE profIncrease for if proficiency gets a boost at the start
+		local nProfMod = tonumber(DB.getValue(nodeSource, "profIncrease", "0"));
+		DB.setValue(nodeChar, "profIncrease", "number", nProfMod);
+	else
+		CharManager.outputUserMessage("SE_char_abilities_message_classadd_abort",DB.getValue(nodeChar, "name", ""));
+		return;
+	end
+end
