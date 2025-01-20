7122b08673121bddd09ad56f662dcbc62829580b
Sean McCarty <28798809+seanmccarty@users.noreply.github.com>
2025-01-19 23:33:22 -0600

    Update randomInit function

--- scripts/manager_combat2.lua
+++ scripts/manager_combat2.lua
@@ -2,13 +2,23 @@ function onInit()
 	CombatManager2.rollRandomInit = rollRandomInit;
 end
 
-function rollRandomInit(nMod, bADV, bDIS)
-	local nInitResult = math.random(10);
-	if bADV and not bDIS then
-		nInitResult = math.max(nInitResult, math.random(10));
-	elseif bDIS and not bADV then
-		nInitResult = math.min(nInitResult, math.random(10));
+function rollRandomInit(tInit)
+	local tSuffix = {};
+	if (tInit.nMod or 0) ~= 0 then
+		table.insert(tSuffix, string.format("(%+d)", tInit.nMod));
 	end
-	nInitResult = nInitResult + nMod;
-	return nInitResult;
+
+	local nInitResult = math.random(10);
+	if tInit.bADV and not tInit.bDIS then
+		local nInitResult2 = math.random(20);
+		table.insert(tSuffix, string.format("[ADV] (DROPPED %d)", math.min(nInitResult, nInitResult2)));
+		nInitResult = math.max(nInitResult, nInitResult2);
+	elseif tInit.bDIS and not tInit.bADV then
+		local nInitResult2 = math.random(20);
+		table.insert(tSuffix, string.format("[DIS] (DROPPED %d)", math.max(nInitResult, nInitResult2)));
+		nInitResult = math.min(nInitResult, nInitResult2);
+	end
+	tInit.sSuffix = table.concat(tSuffix, " ");
+
+	return nInitResult + (tInit.nMod or 0);
 end
